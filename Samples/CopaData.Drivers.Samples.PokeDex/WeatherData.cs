3cd4da8c8875e41f224ee46994f24d3d13eb2a9f
FeliPu <Felixp@copadata.com>
2021-04-25 09:49:24 +0200

    asd

---
+++ Samples/CopaData.Drivers.Samples.PokeDex/WeatherData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CopaData.Drivers.Samples.WeatherForecast
+{
+  public class WeatherData
+  {
+    public string Error { get; set; } = string.Empty;
+    public DateTime DateTime { get; set; }
+    public Dictionary<string, object> Data { get; set; } = new Dictionary<string, object>();
+    public bool HasErrors()
+    {
+      return Error != string.Empty;
+    }
+  }
+}
