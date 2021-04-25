5a43a83f7d5212cc623b9a0c68bf4ea2a1f97375
FeliPu <Felixp@copadata.com>
2021-04-25 22:08:27 +0200

    testing

---
+++ Samples/CopaData.Drivers.Samples.PokeDex/PokeDexApi.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using PokeApiNet;
+
+namespace CopaData.Drivers.Samples.PokeDex
+{
+    public class PokeDexApi
+  {
+        private readonly PokeApiClient PokeClient = new PokeApiClient();
+        Pokemon ditto = await PokeClient.GetResourceAsync<Pokemon>(132);
+
+     /*       return new[]
+      { //Add any key here if more weather information is needed.
+        //Any data is stored in a dictionary in weather data with that key. 
+        //The key needs to match the variable symbolic address in Engineering Studio
+        //to match the value to the variable
+        "Phrase",
+        "IconCode",
+        "Temperature",
+        "TemperatureUnit",
+        "RelativeHumidity",
+        "WindDirectionDegrees",
+        "WindSpeed",
+        "WindSpeedUnit",
+        "CloudCover",
+        "Pressure",
+        "PressureUnit",
+      };*/
+    }
