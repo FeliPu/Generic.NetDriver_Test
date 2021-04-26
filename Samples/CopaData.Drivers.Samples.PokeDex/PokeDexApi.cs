6f33abf9cb31e0a43c3e10932e41def5767d91bd
FeliPu <Felixp@copadata.com>
2021-04-26 18:10:32 +0200

    First sample for height

--- Samples/CopaData.Drivers.Samples.PokeDex/PokeDexApi.cs
+++ Samples/CopaData.Drivers.Samples.PokeDex/PokeDexApi.cs
@@ -9,25 +9,25 @@ using PokeApiNet;
 namespace CopaData.Drivers.Samples.PokeDex
 {
     public class PokeDexApi
-  {
-        private readonly PokeApiClient PokeClient = new PokeApiClient();
-        Pokemon ditto = await PokeClient.GetResourceAsync<Pokemon>(132);
+    {
 
-     /*       return new[]
-      { //Add any key here if more weather information is needed.
-        //Any data is stored in a dictionary in weather data with that key. 
-        //The key needs to match the variable symbolic address in Engineering Studio
-        //to match the value to the variable
-        "Phrase",
-        "IconCode",
-        "Temperature",
-        "TemperatureUnit",
-        "RelativeHumidity",
-        "WindDirectionDegrees",
-        "WindSpeed",
-        "WindSpeedUnit",
-        "CloudCover",
-        "Pressure",
-        "PressureUnit",
-      };*/
+
+        /*       return new[]
+         { //Add any key here if more weather information is needed.
+           //Any data is stored in a dictionary in weather data with that key. 
+           //The key needs to match the variable symbolic address in Engineering Studio
+           //to match the value to the variable
+           "Phrase",
+           "IconCode",
+           "Temperature",
+           "TemperatureUnit",
+           "RelativeHumidity",
+           "WindDirectionDegrees",
+           "WindSpeed",
+           "WindSpeedUnit",
+           "CloudCover",
+           "Pressure",
+           "PressureUnit",
+         };*/
     }
+}
---
+++ Samples/CopaData.Drivers.Samples.PokeDex/PokeDriverExt.cs
@@ -0,0 +1,55 @@
+﻿using CopaData.Drivers.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using PokeApiNet;
+
+namespace CopaData.Drivers.Samples.PokeDex
+{
+    class PokeDriverExt : IDriverExtension
+    {
+        private ILogger Logger { get; set; }
+        private IValueCallback ValueCallback { get; set; }
+        private PokeApiClient pokeclient { get; set; }
+        public Task InitializeAsync(ILogger logger, IValueCallback valueCallback, string configFilePath)
+        {
+            Logger = logger;
+            ValueCallback = valueCallback;
+            pokeclient = new PokeApiClient();
+            return Task.CompletedTask;
+        }
+
+        public async Task ReadAllAsync()
+        {
+            Pokemon hoOh = await pokeclient.GetResourceAsync<Pokemon>("ho-oh");
+            ValueCallback.SetValue("test", hoOh.Height);
+            return;
+        }
+
+        public Task ShutdownAsync()
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task<bool> SubscribeAsync(string symbolicAddress)
+        {
+            return Task.FromResult(true);
+        }
+
+        public Task UnsubscribeAsync(string symbolicAddress)
+        {
+            return Task.FromResult(true);
+        }
+
+        public Task<bool> WriteNumericAsync(string symbolicAddress, double value, DateTime dateTime, StatusBits statusBits)
+        {
+            return Task.FromResult(true);
+        }
+
+        public Task<bool> WriteStringAsync(string symbolicAddress, string value, DateTime dateTime, StatusBits statusBits)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
